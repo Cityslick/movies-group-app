adb44d28b466eb5b39fa4e28c089f9ab15104d77
Aury Rodriguez <arm010286@hotmail.com>
2017-08-18 11:05:10 -0400

    add migration and seeds

---
+++ movies-app/db/migration/migration-081917.sql
@@ -0,0 +1,19 @@
+\c movies_p3_dev
+CREATE TABLE IF NOT EXISTS users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(255) UNIQUE NOT NULL,
+	password_digest TEXT NOT NULL,
+	email  VARCHAR(255)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS movies (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(255),
+	description TEXT,
+	genre VARCHAR(255),
+	user_id INT REFERENCES users(id)
+)
+
+

b4f76902fa33e7e0d0bb71d69716accf58d0dd80
Thomas Santos <thomas@socialeffort.co>
2017-08-18 11:10:48 -0400

    finish app.js

--- movies-app/client/src/App.js
+++ movies-app/client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 class App extends Component {
   constructor() {
@@ -12,7 +15,35 @@ class App extends Component {
       currentMovieId: null,
       movieData: null,
     }
+    this.setPage = this.setPage.bind(this);
   }
-};
+
+  setPage(page) {
+    console.log('click');
+    this.setState({
+      currentPage: page,
+    })
+  }
+
+  decideWhichPage() {
+    switch(this.state.currentPage) {
+      case 'home':
+        return <Home />
+        break;
+        default:
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header setPage={this.setPage} />
+        {this.decideWhichPage()}
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;

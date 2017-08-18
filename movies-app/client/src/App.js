a7fce3ad379fc615e6b6d00944853f7b1934e54f
Thomas Santos <thomas@socialeffort.co>
2017-08-18 10:52:24 -0400

    create constructor with initial state

--- movies-app/client/src/App.js
+++ movies-app/client/src/App.js
@@ -3,19 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      auth: false,
+      user: null,
+      currentPage: 'home',
+      currentMovieId: null,
+      movieData: null,
+    }
   }
-}
+};
 
 export default App;
